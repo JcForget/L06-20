49dfce176b165fb34cd2647756694ace3e3e1d40
JC <forgeje@gmail.com>
2021-09-15 19:30:01 -0400

    À optimiser

--- exercice3.py
+++ exercice3.py
@@ -1,25 +1,48 @@
 
 def decomposer(secondes):
+    # Variables pour remplacer les chiffres
+    # 60 (min)*60(heure)*24(jour)*365(année)
+    uneAnnée = 60*60*24*365
+    # 60 (min)*60 (heure)*24 (jour)* 7 (semaine)
+    septJours = 60*60*24*7
+    # 60 (min)*60 (heure)*24 (jour)
+    unJour = 60*60*24
+    # 60 (min)*60 (heure)
+    uneHeure = 60*60
+    # 60 (min)
+    uneMinute = 60
 
-    # TODO: Assigner à la variable "annees" le nombre d'années
-    annees =
 
-    # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    semaines =
+    # DONE: Assigner à la variable "annees" le nombre d'années
 
-    # TODO: Assigner à la variable "jours" le nombre de jours restants
-    jours =
+    annees = secondes//uneAnnée
+    reste  = secondes % uneAnnée
+    # DONE: Assigner à la variable "semaines" le nombre de semaines restantes
+    if reste != 0:
+        semaines = reste//septJours
+        reste =reste % septJours
 
-    # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    heures =
+    # DONE: Assigner à la variable "jours" le nombre de jours restants
+    if reste != 0:
+        jours = reste//unJour
+        reste =reste % unJour
 
-    # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    minutes =
+    # DONE: Assigner à la variable "heures" le nombre d'heures restantes
+    if reste != 0:
+        heures = reste // uneHeure
+        reste = reste % uneHeure
 
-    # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    secondes =
+    # DONE: Assigner à la variable "minute" le nombre de minutes restantes
+    if reste != 0:
+        minutes = reste // uneMinute
+        reste = reste % uneMinute
 
-    # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
+    # DONE: Assigner à la variable "secondes" le nombre de secondes restantes
+    if reste != 0:
+        secondes = reste
+
+
+    # DONE: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
     print(annees ,semaines ,jours ,heures ,minutes ,secondes)
 
     return (annees ,semaines ,jours ,heures ,minutes ,secondes)
